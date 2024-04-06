83a10c46d56c3dac4839c431ed7b8e0749b0172b
Hugo Luis <hugofranciscoluis@gmail.com>
2024-04-06 12:53:11 -0600

    ADD INVENTARIO READY

--- src/Models/Models/Inventario.java
+++ src/Models/Models/Inventario.java
@@ -1,13 +1,14 @@
 package Models;
-
 import java.util.ArrayList;
+import java.util.List;
 import utils.Messages;
-
 public class Inventario {
     private static Inventario instancia = null;
     private static ArrayList<Maquinas> listaMaquinas = new ArrayList<>();
     private static ArrayList<Equipo> listaEquipos = new ArrayList<>();
-
+    String listaTipo [] = {"0","Sección de Cardio","Sección de Piernas","Sección de Brazos","Sección de Espalda","Sección de Hombros","Seccion de Abdominales","Seccion de pechos"};
+    String listaStatus[] = {"0","FUNCIONA :) ","NO FUNCIONA :( "};
+    String listaSeccion [] = { "0","Mancuerna","Disco","Barras"};
     public Inventario() {}
 
     public static Inventario obtenerInstancia() {
@@ -20,7 +21,6 @@ public class Inventario {
     public ArrayList<Maquinas> getListaMaquinas() {
         return listaMaquinas;
     }
-
     public void setListaMaquinas(Maquinas maquinas) {
         listaMaquinas.add(maquinas);
     }
@@ -28,7 +28,6 @@ public class Inventario {
     public ArrayList<Equipo> getListaEquipos() {
         return listaEquipos;
     }
-
     public void setListaEquipos(Equipo equipo) {
         listaEquipos.add(equipo);
     }
@@ -39,11 +38,13 @@ public class Inventario {
         if (listaMaquinas.size() == 0) {
             System.out.println("No hay máquinas en el inventario.");
         } else {
-            for (Maquinas maquina : listaMaquinas) {
+            for (int i = 0; i < listaMaquinas.size(); i++) {
+                Maquinas maquina = listaMaquinas.get(i);
                 System.out.println(
+                        "ID: " + maquina.getId() + "\n" +
+                        "Tipo: " + listaTipo[maquina.getTipo()] + "\n" +
                         "Nombre: " + maquina.getNombre() + "\n" +
-                        "Estatus: " + maquina.getEstatus() + "\n" +
-                        "Tipo: " + maquina.getTipo()
+                        "Estatus: " + listaStatus[maquina.getEstatus()]
                 );
                 msm.separador();
             }
@@ -53,18 +54,20 @@ public class Inventario {
         if (listaEquipos.size() == 0) {
             System.out.println("No hay equipos en el inventario.");
         } else {
-            for (Equipo equipo : listaEquipos) {
+            for (int i = 0; i < listaEquipos.size(); i++) {
+                Equipo equipo = listaEquipos.get(i);
                 System.out.println(
-                        "Sección: " + equipo.getSeccion() + "\n" +
+                        "ID: " + equipo.getId() + "\n" +
+                        "Sección: " + listaSeccion[equipo.getSeccion()] + "\n" +
                         "Nombre: " + equipo.getNombre() + "\n" +
                         "Peso (kg): " + equipo.getPeso() + "\n" +
-                        "Estatus: " + equipo.getEstatus()
+                        "Estatus: " + listaStatus[equipo.getEstatus()]
                 );
                 msm.separador();
             }
         }
     }
-
+    
     public void buscarAparatos() {
     
     }
