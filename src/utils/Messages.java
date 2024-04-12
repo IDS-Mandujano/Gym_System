0ea0ddcfdc9a0a8bd814c30b786dd168ca97f765
IDS-Mandujano <jesusgutierrez191105@gmail.com>
2024-04-12 00:30:43 -0600

    UPDATE Messages

--- src/utils/Messages.java
+++ src/utils/Messages.java
@@ -3,23 +3,23 @@ package utils;
 public class Messages {
     public void mensajeSalida(){System.out.println("Saliendo.....");}
     public void separador(){System.out.println("------------------------------------");}
-    public void mensageErrorRango(){System.out.println("Eliga una opción dentro del rango");}
+    public void mensageErrorRango(){System.out.println("Eliga una opción dentro del rango.");}
 
-    public void eliminar(){System.out.println("Ingrese el ID del cliente que desea eliminar");}
-    public void editar(){System.out.println("Ingrese el ID del cliente que desea editar");}
-    public void buscar(){System.out.println("Ingrese el ID del cliente que desea buscar");}
+    public void eliminar(){System.out.println("Ingrese el ID del cliente que desea eliminar.");}
+    public void editar(){System.out.println("Ingrese el ID del cliente que desea editar.");}
+    public void buscar(){System.out.println("Ingrese el ID del cliente que desea buscar.");}
 
-    public void buscarMaquina(){System.out.println("Ingrese el ID de la maquina a buscar");}
-    public void buscarEquipo(){System.out.println("Ingrese el id ID del equipo a buscar");}
-    public void idMaquinaEditar(){System.out.println("Ingrese el id de la maquina a editar");}
-    public void idEquipoEditar(){System.out.println("Ingrese el id del equipo a editar");}
-    public void editarMaquinaNombre(){System.out.println("Ingrese el nombre nuevo");}
+    public void buscarMaquina(){System.out.println("Ingrese el ID de la maquina a buscar.");}
+    public void buscarEquipo(){System.out.println("Ingrese el id ID del equipo a buscar.");}
+    public void idMaquinaEditar(){System.out.println("Ingrese el id de la maquina a editar.");}
+    public void idEquipoEditar(){System.out.println("Ingrese el id del equipo a editar.");}
+    public void editarMaquinaNombre(){System.out.println("Ingrese el nombre nuevo.");}
     public void nuevoNombre() {System.out.print("Ingrese el nuevo nombre: ");}
-    public void idMaquinaEliminar(){System.out.println("Ingrese el id de la maquina a eliminar");}
-    public void idEquipoEliminar(){System.out.println("Ingrese el id del equipo a elimiar");}
-    public void mensajePeso(){System.out.println("Ingrese un peso dentro del rango");}
+    public void idMaquinaEliminar(){System.out.println("Ingrese el id de la maquina a eliminar.");}
+    public void idEquipoEliminar(){System.out.println("Ingrese el id del equipo a elimiar.");}
+    public void mensajePeso(){System.out.println("Ingrese un peso dentro del rango.");}
     public void mensajeNotFound(){System.out.println(
-        "Clientes no registrados\n"+
-        "Favor de ingresar un cliente nuevo");
+        "Clientes no registrados.\n"+
+        "Favor de ingresar un cliente nuevo.");
     }
 }
